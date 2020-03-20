a044d22d813af43462ccd1518c569935ddbafcbc
thivanka wijesooriya <thivankawijesooriya@gmail.com>
2020-03-21 02:33:25 +0530

    spring core updt

---
+++ 02.Spring Core/Spring Basics/08.Reference Object - Constructor Based Injection - annotation/Faculty.java
@@ -0,0 +1,34 @@
+package com.demo.model;
+
+public class Faculty {
+
+	private int id;
+	private String name;
+
+	public Faculty() {
+
+	}
+
+	public Faculty(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
