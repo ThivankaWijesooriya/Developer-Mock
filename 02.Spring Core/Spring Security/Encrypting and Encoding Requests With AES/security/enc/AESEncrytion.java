c6cebf0c0d93271af641a20664151e873e56186e
thivanka wijesooriya <thivankawijesooriya@gmail.com>
2021-06-19 13:57:09 +0530

    aes security update

--- 02.Spring Core/Spring Security/Encrypting and Encoding Requests With AES/security/enc/AESEncrytion.java
+++ 02.Spring Core/Spring Security/Encrypting and Encoding Requests With AES/security/enc/AESEncrytion.java
@@ -1,10 +1,9 @@
 package com.synapsys.gp.security.enc;
 
-import java.util.Base64;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
 
 import org.springframework.stereotype.Component;
 
@@ -13,9 +12,12 @@ public class AESEncrytion {
 
 	private static final String key = "abv3eksuGZM8r/x/bXFdmz++b1AIj97x0ui90orQdHM=";
 	private static final String initVector = "u/JWlbtAPL/fx7ipyZc3oQ==";
-	private static final byte[] base64key = Base64.getDecoder().decode(key);
-	private static final byte[] base64InitVector = Base64.getDecoder().decode(initVector);
-
+	
+	//private static final byte[] base64key = Base64.getDecoder().decode(key);
+	private static final byte[] base64key = Base64.decodeBase64(key.getBytes());
+	
+	//private static final byte[] base64InitVector = Base64.getDecoder().decode(initVector);
+	private static final byte[] base64InitVector = Base64.decodeBase64(initVector.getBytes());
 
 	public String encrypt(String value) {
 		try {
@@ -28,7 +30,8 @@ public class AESEncrytion {
 
 			byte[] encrypted = cipher.doFinal(value.getBytes());
 
-			String encryptedString = Base64.getEncoder().encodeToString(encrypted);
+			//String encryptedString = Base64.getEncoder().encodeToString(encrypted);
+			String encryptedString = new String(Base64.encodeBase64(encrypted));
 
 			System.out.println("Original String  - " + value);
 			System.out.println("Encrypted String - " + encryptedString);
@@ -50,7 +53,9 @@ public class AESEncrytion {
 
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
 			cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-			byte[] byteArr = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+			
+			//byte[] byteArr = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+			byte[] byteArr = cipher.doFinal(Base64.decodeBase64(encrypted.getBytes()));
 			
 			String decryptedString = new String(byteArr);
 			
